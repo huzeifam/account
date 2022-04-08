deb327731b54bf47f08aefabf0fb838d6fdca47f
huzeifam <huzeifa.marzouka@adesso.de>
2022-04-08 15:20:22 +0200

    Initial commit

---
+++ src/main/java/com/example/account/model/AccountCreateRequest.java
@@ -0,0 +1,19 @@
+package com.example.account.model;
+
+
+import java.time.LocalDateTime;
+
+public class AccountCreateRequest {
+
+    private Integer customerNo;
+
+
+    public AccountCreateRequest(Integer customerNo, String iban,
+                                Double balanceInEuro, LocalDateTime startDate) {
+        this.customerNo = customerNo;
+
+    }
+    public Integer getCustomerNo() { return customerNo; }
+
+
+}
